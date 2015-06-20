0673049110d6c46294dd84b3c55429d723b053b4
DomTheDeveloper <dominic.dabish@gmail.com>
2015-06-20 13:53:26 -0400

    reader uploaded

---
+++ 4c/emoji-reader/compile_reader.bat
@@ -0,0 +1,1 @@
+g++ reader.cpp -std=c++11 -o reader -Wall -Wextra -pedantic
---
+++ 4c/emoji-reader/reader.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <unordered_set>
+
+using namespace std;
+
+static std::unordered_set<string> emojis;
+
+int main () {
+  string line;
+  ifstream myfile ("emoji-data.txt");
+  if (myfile.is_open())
+  {
+    while ( getline (myfile,line) )
+    {
+	  if (line.at(0)=='#')
+		  continue;
+	  
+	  line = line.substr(0, line.find(";", 1));
+	  
+	  int spaces = 0;
+	  for(char& c : line) {
+	    if (c == ' ')
+			spaces++;
+      }
+	  
+	  if (spaces >= 2)
+		  continue;
+	  
+	  line.insert(0, "0x");
+	  
+	  emojis.insert(line);
+	  
+      cout << line << '\n';
+    }
+    myfile.close();
+	
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
